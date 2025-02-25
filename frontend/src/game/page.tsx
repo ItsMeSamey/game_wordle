15c4625fe2efe1c2aac545ec038d283fbd601f0a
ItsMeSamey <sameychain5041@gmail.com>
2025-02-25 05:55:26 +0530

    updated game logix

--- frontend/src/game/page.tsx
+++ frontend/src/game/page.tsx
@@ -7,6 +7,7 @@ import { showError, showServerError } from '../utils/toast'
 import LoadingScreen from '../pages/loading_screen'
 import { addAllWords, addWords, calcDiff, getRandomWord, setDone, WordLength } from './words'
 import { LocalstorageStore } from '../utils/store'
+import ModeToggleGroup from '../components/mode_toggle_group'
 
 // Green, Yellow, Red respectively
 type WordleStringState = 'g' | 'y' | 'r'
@@ -263,6 +264,7 @@ export default function Wordle() {
   }).catch(showServerError)
 
   return <Show when={!loading()} fallback={<LoadingScreen pageString='Loading Words' />}>
+    <ModeToggleGroup />
     {WordleModel(wordLength())}
   </Show>
 }
