2f6bd6e894abda49eb4bdff3b19f40ad5039c039
ItsMeSamey <sameychain5041@gmail.com>
2025-02-26 12:14:06 +0530

    added appcache

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,36 @@ import tailwindcss from '@tailwindcss/vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
 import path from 'path'
+import { Plugin } from 'vite'
+import * as fs from 'fs'
+
+function vitePluginAppcache(options: {
+  fileName?: string; // Customize the manifest file name (default: 'appcache.manifest')
+} = {}): Plugin {
+  const fileName = options.fileName || 'appcache.manifest'
+
+  return {
+    name: 'vite-plugin-appcache',
+    writeBundle: async (outputOptions) => {
+      const outputPath = path.resolve(outputOptions.dir || 'dist', fileName); // Default to 'dist' if outputOptions.dir is not defined
+
+      try {
+        fs.writeFileSync(outputPath, `CACHE MANIFEST
+# Timestamp: ${new Date().getTime()}
+
+CACHE:
+index.html
+
+NETWORK:
+*
+`)
+        console.log(`AppCache manifest file '${fileName}' generated at: ${outputPath}`)
+      } catch (error) {
+        console.error(`Error generating AppCache manifest file:`, error)
+      }
+    },
+  }
+}
 
 export default defineConfig({
   plugins: [
@@ -24,11 +54,12 @@ export default defineConfig({
       sortClassName: true,
       useShortDoctype: true,
     }),
+    vitePluginAppcache(),
   ],
 
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "./submodules/solid-ui/apps/docs/src/"),
+      '~': path.resolve(__dirname, './submodules/solid-ui/apps/docs/src/'),
     }
   },
 
@@ -49,3 +80,4 @@ export default defineConfig({
     },
   },
 })
+
